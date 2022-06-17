da7169f69895e14871388b79db595e697745838c
LaiTsz <laitszchung706@gmail.com>
2022-06-17 20:49:01 +0800

    finish game and playRound function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,48 @@
 let selection=['rock','paper','scissor'];
+let win=0;
+let lose=0;
 //return either rock paper scissor
 function computerPlay(){
     return selection[Math.floor(Math.random()*3)];
 }
 console.log(computerPlay());
+function playRound(playerSelection,computerSelection){
+    playerSelection=playerSelection.toLowerCase();
+    if(playerSelection===computerSelection)
+    {return 'Draw';}
+    else if(playerSelection==='scissor'&&computerSelection==='paper'
+    ||playerSelection==='paper'&&computerSelection==='rock'||playerSelection==='rock'&&computerSelection==='scissor')
+    {
+        return 'win';
+    }
+    else 
+    {
+        return 'lose';
+    }
+}
+function game(){
+    for(let i=0;i<5;i++)
+    {
+        let player=prompt('rock paper scissor','');
+        let computer=computerPlay();
+        //console.log(playRound(prompt('rock paper scissor',''),computerPlay()));
+        if(playRound(player,computer)==='win')
+        {
+            win++;
+            console.log(`Round ${i+1},player win, player ${player} beat computer ${computer}`);
+        }
+        else if(playRound(player,computer)==='lose')
+        {
+            lose++;
+            console.log(`Round ${i+1},computer win, computer ${computer} beat player ${player}`);
+        }
+        else
+            console.log(`Round ${i+1},Draw`);
+    }
+    if(win>lose)
+    {
+        console.log('you win');
+    }
+    else
+        console.log('you lose');
+}
